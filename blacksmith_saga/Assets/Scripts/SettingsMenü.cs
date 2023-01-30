14efc5818fdff66977608c9ba83dbeba001b1bc7
Adrian Hettstedt <adrian.hettstedt@fh-erfurt.de>
2023-01-30 17:04:10 +0100

    MainMenü

--- blacksmith_saga/Assets/Scripts/SettingsMenü.cs
+++ blacksmith_saga/Assets/Scripts/SettingsMenü.cs
@@ -1,9 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
+    public void AudioMixer mainMixer;
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
